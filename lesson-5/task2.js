a22da46916c2cc883d53c7593d9d3f931f70268d
Andrii Poshtaruk <poshta.andruk@gmail.com>
2025-02-06 23:11:16 +0200

    Fixed the dz

--- lesson-5/task2.js
+++ lesson-5/task2.js
@@ -6,20 +6,20 @@ let averageGrade = Math.floor(Math.random() * 100) + 1;
 console.log(`Середня оцінка студента: ${averageGrade}`);
 
 switch (averageGrade) {
-    case (averageGrade <=91 && averageGrade >=100):
-        console.log('Відмінно');
-        break;
-    case (averageGrade <=81 && averageGrade >=90):
-        console.log('Дуже добре');
-        break;
-    case (averageGrade <=71 && averageGrade >=80):
-        console.log('Добре');
-        break;
-    case (averageGrade <=60 && averageGrade >=70):
-        console.log('Задовільно');
-        break;
+    case (averageGrade >=91 && averageGrade <=100):
+        console.log('Відмінно')
+        break
+    case (averageGrade >=81 && averageGrade <=90):
+        console.log('Дуже добре')
+        break
+    case (averageGrade >=71 && averageGrade <=80):
+        console.log('Добре')
+        break
+    case (averageGrade >=60 && averageGrade <=70):
+        console.log('Задовільно')
+        break
     default:
-        console.log('Незадовільно');
+        console.log('Незадовільно')
 }
 
 
