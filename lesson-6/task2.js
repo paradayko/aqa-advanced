687296a4d14935315170cfced8cd52c7264f82aa
Andrii Poshtaruk <poshta.andruk@gmail.com>
2025-02-12 18:01:32 +0200

    Did the sixth homework with the functions

---
+++ lesson-6/task2.js
@@ -0,0 +1,11 @@
+const personIsAdult = function(age) {
+    if (age>=18) {
+        return true
+    } else {
+        return false
+    } 
+
+}
+
+console.log(personIsAdult(15))
+console.log(personIsAdult(25))
